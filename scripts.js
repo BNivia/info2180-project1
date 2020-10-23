d5cb422e813dba47ff298c87085ce766783def46
BNivia <nobenjamin19@gmail.com>
2020-10-22 23:08:51 -0500

    Added thank you message and alternative error message with js.

--- scripts.js
+++ scripts.js
@@ -2,25 +2,21 @@
 //Naomi Benjmain
 
 document.addEventListener('DOMContentLoaded', function(){
-   let msg = document.getElementsByClassName("message");
-   let form = document.querySelector("form");
-   let email = document.querySelector("input").textContent;
+   let msg = document.getElementsByClassName("message")[0];
+   let btn = document.getElementsByTagName("button")[0];
+   let email = document.getElementById("email");
    
  
-   form.addEventListener('submit', (e) =>{
-       if (email === '' || email === null){
+   btn.addEventListener('click', (event) =>{
+       event.preventDefault();
+       if (email.value === '' || email.value === null){
            msg.textContent = 'Please enter a valid email address.';
-           console.log("lol works");
+           //console.log("200");
        }
        else{
-           msg.textContent = 'Thank you!';
-       }
-        //e.preventDefault();
+           msg.textContent = `Thank you! Your email address, ${email.value}, has been added to our mailing list.`;
+           //console.log("200");
+        }
    });
     console.log("Yo okay. We loaded JS.")
-    
 }, false);
-
-
-
-//var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
